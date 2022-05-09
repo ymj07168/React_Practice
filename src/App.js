742d91c0062f938e566942d249df5820ef13d0e0
ymj07168 <ymj07168@gmail.com>
2022-05-09 16:31:44 +0900

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
-import "./style.css";
+import "./App.css";
+function Header(props){
+  console.log('props', props, props.title)
+  return <header>
+    <h1><a href="/">{props.title}</a></h1>
+  </header>
+}
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+}
+function Article(){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Header title="REACT"></Header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
+      <Article title="Hi" body="Hello, WEB"></Article>
     </div>
   );
 }
